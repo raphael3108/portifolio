21ff109e2f5b22ce171b1b3ee9ce28f9d54cf26c
Raphael germano <raphael.silva31@escola.pr.gov.br>
2025-05-15 10:04:06 -0300

    Add files via upload

---
+++ Exercicio1/script.js
@@ -0,0 +1,13 @@
+let num1;
+let num2;
+let resultado;
+
+
+num1 = Number(window.prompt("Digite o primeiro numero : "));
+num2 = Number(window.prompt("Digite o segudo numero : "));
+
+resultado = num1 + num2;
+alert(resultado);
+
+
+

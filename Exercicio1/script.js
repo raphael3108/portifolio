c601bc3083bfbfe7220751e1c993ee16eacbf371
Raphael germano <raphael.silva31@escola.pr.gov.br>
2025-05-15 12:26:08 -0300

    Add files via upload

--- Exercicio1/script.js
+++ Exercicio1/script.js
@@ -1,13 +1,27 @@
 let num1;
 let num2;
 let resultado;
-
+let parOuImpar
 
 num1 = Number(window.prompt("Digite o primeiro numero : "));
+
 num2 = Number(window.prompt("Digite o segudo numero : "));
 
 resultado = num1 + num2;
-alert(resultado);
 
+/*if(resultado > 10) {
+    alert("o número" + resultado + "é maior do que 10 ! ");
+} else {
+    alert("o número" + resultado + ",é menor ou igual a que 10 ! ");
+}*/
 
+parOuImpar = resultado % 2;
 
+if(parOuImpar == 0) {
+
+    alert("o numero " + resultado + " é par ") ;
+
+} else {
+
+    alert("o numero " + resultado + " é impar ") ;
+}

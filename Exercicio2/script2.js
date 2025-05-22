98c40cfc1e0df9e25854443a002cd122306c11d2
Raphael germano <raphael.silva31@escola.pr.gov.br>
2025-05-22 09:53:01 -0300

    Add files via upload

---
+++ Exercicio2/script2.js
@@ -0,0 +1,47 @@
+let resultado
+let num1
+let num2
+
+function somar(){
+    alert("")
+    num1 = Number(window.prompt("Digite o primeiro numero :  "));
+    num2 = Number(window.prompt("Digite o segundo numero :  "));
+
+    resultado = num1 + num2;
+
+    alert("o resultado é : " + resultado + "");
+
+}
+
+function subtrair(){
+    alert("")
+    num1 = Number(window.prompt("Digite o primeiro numero :  "));
+    num2 = Number(window.prompt("Digite o segundo numero :  "));
+
+     resultado = num1 - num2;
+
+     alert("o resultado é : " + resultado + "");
+
+}
+
+function dividir(){
+    alert("")
+    num1 = Number(window.prompt("Digite o primeiro numero : "));
+    num2 = Number(window.prompt("Digite o segundo numero : "));
+
+    resultado = num1 / num2;
+
+    alert("o resultado é : " + resultado + "");
+
+}
+
+function multiplicar(){
+    alert("")
+    num1 = Number(window.prompt("Digite o primeiro numero : "));
+    num2 = Number(window.prompt("Digite o segundo numero : "));
+
+     resultado = num1 * num2;
+
+     alert("o resultado é : " + resultado + "");
+}
+
